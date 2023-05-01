9b79aeb43d8ca0a4bc68793ff503f708ff3ea9ff
tuwebencaliente <131494155+tuwebencaliente@users.noreply.github.com>
2023-05-01 12:08:29 -0600

    Add files via upload

--- php/mail.php
+++ php/mail.php
@@ -1,11 +1,32 @@
 <?php 
 	$nombre = $_POST['nombre'];
     $apellidos = $_POST['apellidos'];
-	$email = $_POST['email'];
-	$msj = "Nombre: ".$nombre."<br> Apellidos:".$apellidos."Email:" .$email. "<br> Mensaje:".$_POST['mensaje'];
+	$correo = $_POST['correo'];
+    $msj = $_POST['msj'];
+	// $msj = "Nombre: ".$nombre."<br> Apellidos:".$apellidos."Email:" .$email. "<br> Mensaje:".$_POST['mensaje'];
 
 
-	if(mail('gcamilo_98@live.com', $asunto, $msj)){
-		echo "Correo enviado";
-	}
+	// if(mail('gcamilo_98@live.com', $asunto, $msj)){
+	// 	echo "Correo enviado";
+	// }
+
+    $header = 'From: '.$correo. "\r\n";
+    $header .= "X-Mailer: PHP/" .phpversion(). "\r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text/plain";
+
+    $message = "Este mensaje fue enviado por: " .$nombre + $apellidos. "\r\n";
+    $message .= "Su email es: " .$correo. "\r\n";
+    $message .= "Mensaje: " .$_POST['message'] . "\r\n";
+    $message .= "Enviado el día: " .date('d/m/Y', time());
+
+    $para = 'gcamilo_98@live.com'; 
+    $asunto = 'Tu web en caliente';
+
+    if (mail($para, $asunto, utf8_decode($message), $header)){
+        echo "Correo enviado!";
+    }
+
+    header ("Location: index.html");
+
  ?>
